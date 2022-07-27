0f7392c3d7db82f24e9ebd1a00b20e7a39d11981
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-07-27 19:52:28 +1000

    Solve Day 1 Part 1

--- test/examples.py
+++ test/examples.py
@@ -1,4 +1,4 @@
 """
 This module contains additional test methods used to test AOC 2017 solution
-code against example inputs.
+code against example inputs. Test methods utilising pytest library.
 """
--- test/solutions.py
+++ test/solutions.py
@@ -1,4 +1,15 @@
 """
 This module contains the test methods used to test the actual solutions for
-each of the solved AOC 2017 problem parts.
+each of the solved AOC 2017 problem parts. Test methods utilising pytest
+library.
 """
+
+from src.solutions import day01
+
+def test_day01_part1():
+    """
+    Solution test method for AOC 2017 Day 1 Part 1.
+    """
+    input_data = day01.process_input_file()
+    solution = day01.solve_part1(input_data)
+    assert solution == 1150
