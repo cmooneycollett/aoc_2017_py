06e46025a8ee1497414e41ee0cd35f6c00354803
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-07-27 19:40:55 +1000

    Add project folder and file template structure

---
+++ template/day00.py
@@ -0,0 +1,25 @@
+"""
+Solutions for AOC 2017 Day ##.
+"""
+
+
+def process_input_file():
+    """
+    Processes the AOC 2017 Day ## input file into the format required by the
+    solver functions. ###
+    """
+    return NotImplemented
+
+
+def solve_part1(_input_data):
+    """
+    Solves AOC 2017 Day ## Part 1 // ###
+    """
+    return NotImplemented
+
+
+def solve_part2(_input_data):
+    """
+    Solves AOC 2017 Day ## Part 2 // ###
+    """
+    return NotImplemented
