75febf378e1443b421b3bbc2ea05122d8b7abe6c
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-07-28 20:43:17 +1000

    Solve Day 2 Part 1

--- test/solutions.py
+++ test/solutions.py
@@ -4,7 +4,8 @@ each of the solved AOC 2017 problem parts. Test methods utilising pytest
 library.
 """
 
-from src.solutions import day01
+from src.solutions import day01, day02
+
 
 def test_day01_part1():
     """
@@ -14,6 +15,7 @@ def test_day01_part1():
     solution = day01.solve_part1(input_data)
     assert solution == 1150
 
+
 def test_day01_part2():
     """
     Solution test method for AOC 2017 Day 1 Part 1.
@@ -21,3 +23,12 @@ def test_day01_part2():
     input_data = day01.process_input_file()
     solution = day01.solve_part2(input_data)
     assert solution == 1064
+
+
+def test_day02_part1():
+    """
+    Solution test method for AOC 2017 Day 2 Part 1.
+    """
+    input_data = day02.process_input_file()
+    solution = day02.solve_part1(input_data)
+    assert solution == 45158
