e79c8aac0a1c09bf85ca872beed59e0076e10002
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-07-31 10:42:51 +1000

    Solve Day 7 Part 2

--- test/test_examples.py
+++ test/test_examples.py
@@ -2,3 +2,23 @@
 This module contains additional test methods used to test AOC 2017 solution
 code against example inputs. Test methods utilising pytest library.
 """
+
+from src.solutions import day07
+
+
+def test_day07_part1_ex01():
+    """
+    Test method for AOC 2017 Day 7 Part 1 using example input 1.
+    """
+    input_data = day07.process_input_file("./input/examples/day07_ex01.txt")
+    solution = day07.solve_part1(input_data)
+    assert solution == "tknk"
+
+
+def test_day07_part2_ex01():
+    """
+    Test method for AOC 2017 Day 7 Part 1 using example input 1.
+    """
+    input_data = day07.process_input_file("./input/examples/day07_ex01.txt")
+    solution = day07.solve_part2(input_data)
+    assert solution == 60
