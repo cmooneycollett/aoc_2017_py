fdb84ff90fc6374bf10fb775fa4ba995f86e0ca5
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-08-02 20:28:08 +1000

    Solve Day 10 Part 2

--- src/solutions/day10.py
+++ src/solutions/day10.py
@@ -24,11 +24,33 @@ def solve_part1(input_string):
     return strand[0] * strand[1]
 
 
-def solve_part2(_input_string):
+def solve_part2(input_string):
     """
-    Solves AOC 2017 Day 10 Part 2 // ###
+    Solves AOC 2017 Day 10 Part 2 // Determines the knot hash of the input
+    string, including input processing (length sequence suffix append), 64
+    rounds of knot hash algorithm and output processing (dense hash calculation
+    and conversion to hexadecimal string).
     """
-    return NotImplemented
+    # Input processing
+    lengths = [ord(char) for char in input_string]
+    lengths.extend([17, 31, 73, 47, 23])
+    # Apply 64 rounds of knot hash algorithm
+    strand = list(range(256))
+    cursor = 0
+    skip = 0
+    for _ in range(64):
+        (strand, cursor, skip) = apply_knot_hash(strand, lengths, cursor, skip)
+    # Convert to dense hash
+    dense_hash = []
+    for block in range(16):
+        block_start = block * 16
+        xor_result = strand[block_start]
+        for delta in range(1, 16):
+            xor_result ^= strand[block_start + delta]
+        dense_hash.append(xor_result)
+    # Convert dense hash to hexidecimal representation
+    hex_digest = [str(hex(block))[2:] for block in dense_hash]
+    return "".join(hex_digest)
 
 
 def apply_knot_hash(start_strand, lengths, start_cursor=0, start_skip=0):
@@ -51,6 +73,6 @@ def apply_knot_hash(start_strand, lengths, start_cursor=0, start_skip=0):
             index = (cursor + delta) % len(strand)
             strand[index] = segment[delta]
         # Update cursor location and increment skip value
-        cursor += length + skip
+        cursor = (cursor + length + skip) % len(strand)
         skip += 1
     return (strand, cursor, skip)
