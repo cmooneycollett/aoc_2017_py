be4f627aa80834c28c52bd37523823a3430b5894
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-08-04 20:04:25 +1000

    Solve Day 12 Part 2

--- src/solutions/day12.py
+++ src/solutions/day12.py
@@ -30,8 +30,30 @@ def solve_part1(program_conns):
     Solves AOC 2017 Day 12 Part 1 // Calculates the number of programs that are
     in the group that contains program ID 0.
     """
-    visit_queue = deque(program_conns[0])
-    visited = set([0])
+    return len(find_programs_in_group(program_conns, 0))
+
+
+def solve_part2(program_conns):
+    """
+    Solves AOC 2017 Day 12 Part 2 // Calculates the total number of separate
+    groups represented by the given program connections.
+    """
+    visited = set()
+    group_count = 0
+    for program in program_conns:
+        if program not in visited:
+            group_members = find_programs_in_group(program_conns, program)
+            visited.update(group_members)
+            group_count += 1
+    return group_count
+
+
+def find_programs_in_group(program_conns, program_group):
+    """
+    Finds the set of programs that are in the given program group.
+    """
+    visit_queue = deque(program_conns[program_group])
+    visited = set([program_group])
     while len(visit_queue) > 0:
         # Visit the next program
         program = visit_queue.popleft()
@@ -40,11 +62,4 @@ def solve_part1(program_conns):
         for next_program in program_conns[program]:
             if next_program not in visited:
                 visit_queue.append(next_program)
-    return len(visited)
-
-
-def solve_part2(_program_conns):
-    """
-    Solves AOC 2017 Day 12 Part 2 // ###
-    """
-    return NotImplemented
+    return visited
