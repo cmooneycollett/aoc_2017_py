06b140349a7f162fc50d949962ed3c0f1d3fbb9c
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-08-05 20:47:08 +1000

    Refactor knot hash algos into cryptography module

--- src/solutions/day10.py
+++ src/solutions/day10.py
@@ -2,6 +2,8 @@
 Solutions for AOC 2017 Day 10.
 """
 
+from src.utils.cryptography import calculate_knot_hash, calculate_sparse_hash
+
 
 def process_input_file(filepath="./input/day10.txt"):
     """
@@ -20,59 +22,13 @@ def solve_part1(input_string):
     hash algorithm for the given number of lengths.
     """
     lengths = [int(num) for num in input_string.split(",")]
-    (strand, _, _) = apply_knot_hash(list(range(256)), lengths)
+    (strand, _, _) = calculate_sparse_hash(list(range(256)), lengths)
     return strand[0] * strand[1]
 
 
 def solve_part2(input_string):
     """
-    Solves AOC 2017 Day 10 Part 2 // Determines the knot hash of the input
-    string, including input processing (length sequence suffix append), 64
-    rounds of knot hash algorithm and output processing (dense hash calculation
-    and conversion to hexadecimal string).
+    Solves AOC 2017 Day 10 Part 2 // Calculates the knot hash for the given
+    string.
     """
-    # Input processing
-    lengths = [ord(char) for char in input_string]
-    lengths.extend([17, 31, 73, 47, 23])
-    # Apply 64 rounds of knot hash algorithm
-    strand = list(range(256))
-    cursor = 0
-    skip = 0
-    for _ in range(64):
-        (strand, cursor, skip) = apply_knot_hash(strand, lengths, cursor, skip)
-    # Convert to dense hash
-    dense_hash = []
-    for block in range(16):
-        block_start = block * 16
-        xor_result = strand[block_start]
-        for delta in range(1, 16):
-            xor_result ^= strand[block_start + delta]
-        dense_hash.append(xor_result)
-    # Convert dense hash to hexidecimal representation
-    hex_digest = [str(hex(block))[2:] for block in dense_hash]
-    return "".join(hex_digest)
-
-
-def apply_knot_hash(start_strand, lengths, start_cursor=0, start_skip=0):
-    """
-    Applies a single iteration of the knot hash to the given strand using the
-    given length values. Returns the resulting strand after single knot hash
-    round, the final cursor value and the final skip value.
-    """
-    strand = list(start_strand)
-    cursor = start_cursor
-    skip = start_skip
-    for length in lengths:
-        # Reverse target segment of list of current length from cursor
-        segment = []
-        for delta in range(length):
-            index = (cursor + delta) % len(strand)
-            segment.append(strand[index])
-        segment.reverse()
-        for delta in range(length):
-            index = (cursor + delta) % len(strand)
-            strand[index] = segment[delta]
-        # Update cursor location and increment skip value
-        cursor = (cursor + length + skip) % len(strand)
-        skip += 1
-    return (strand, cursor, skip)
+    return calculate_knot_hash(input_string)
