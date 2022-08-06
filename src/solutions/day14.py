fa20af1233d3293c56874ef73cb973a3768884bd
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-08-06 13:56:45 +1000

    Solve Day 14 Part 2

--- src/solutions/day14.py
+++ src/solutions/day14.py
@@ -2,6 +2,8 @@
 Solutions for AOC 2017 Day 14.
 """
 
+from collections import deque
+from src.utils.cartography import Location2D
 from src.utils.cryptography import calculate_knot_hash
 
 
@@ -23,12 +25,69 @@ def solve_part1(key):
     used_count = 0
     for row in range(128):
         knot_hash = calculate_knot_hash(f"{key}-{row}")
-        used_count += bin(int(knot_hash, 16))[2:].count("1")
+        for hex_char in knot_hash:
+            used_count += bin(int(hex_char, 16))[2:].zfill(4).count("1")
     return used_count
 
 
-def solve_part2(_key):
+def solve_part2(key):
     """
-    Solves AOC 2017 Day 14 Part 2 // ###
+    Solves AOC 2017 Day 14 Part 2 // Calculates the number of regions in the
+    128-by-128 disk grid given the key string.
     """
-    return NotImplemented
+    # Populate the disk grid - "0" for free space, "1" for used space
+    disk_array = []
+    for row in range(128):
+        disk_row = []
+        knot_hash = calculate_knot_hash(f"{key}-{row}")
+        for hex_char in knot_hash:
+            bits = bin(int(hex_char, 16))[2:].zfill(4)
+            disk_row.extend(bits)
+        disk_array.append(disk_row)
+    # Find regions using BFS method
+    visited = set()
+    regions = 0
+    for row in range(128):
+        for col in range(128):
+            # Free space is not in a region
+            if disk_array[row][col] == "0":
+                continue
+            # Check for the start of new region
+            loc = Location2D(col, row)
+            if loc not in visited:
+                region_members = find_region_members(disk_array, loc)
+                visited.update(region_members)
+                regions += 1
+    return regions
+
+
+def find_region_members(disk_array, loc2d):
+    """
+    Finds the members of the region including the given 2D-point.
+    """
+    visited = set()
+    state_queue = deque([loc2d])
+    while len(state_queue) > 0:
+        location = state_queue.popleft()
+        visited.add(location)
+        for next_location in find_adjacent_used_locations(disk_array, location):
+            if next_location not in visited:
+                state_queue.append(next_location)
+    return visited
+
+
+def find_adjacent_used_locations(disk_array, loc2d):
+    """
+    Yields the used locations in the disk grid that are adjacent to the given
+    2D-point (diagonals not included).
+    """
+    for delta in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+        new_x = loc2d.loc_x + delta[0]
+        new_y = loc2d.loc_y + delta[1]
+        # Skip locations that are outside of the disk array
+        if new_x < 0 or new_y < 0 or new_x >= 128 or new_y >= 128:
+            continue
+        # Skip locations that are marked as "free" space
+        if disk_array[new_y][new_x] == "0":
+            continue
+        yield Location2D(new_x, new_y)
