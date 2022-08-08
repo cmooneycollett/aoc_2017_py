4b3146f5523627713ad5db32d0061bcffa0e710d
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-08-08 21:27:13 +1000

    Solve Day 17 Part 2

--- src/solutions/day17.py
+++ src/solutions/day17.py
@@ -20,15 +20,25 @@ def solve_part1(steps):
     spinlock = [0]
     cursor = 0
     for code in range(1, 2018):
-        print(f"[+] Inserted code {code} at index {cursor + 1}")
         spinlock.insert(cursor + 1, code)
         cursor = (cursor + 1 + steps) % len(spinlock)
     target_index = (spinlock.index(2017) + 1) % len(spinlock)
     return spinlock[target_index]
 
 
-def solve_part2(_steps):
+def solve_part2(steps):
     """
-    Solves AOC 2017 Day 17 Part 2 // ###
+    Solves AOC 2017 Day 17 Part 2 // Returns the value after the value 0 the
+    moment that the value 50000000 is inserted.
     """
-    return NotImplemented
+    cap = 50000000      # Stop after 50 million "inserts" into spinlock
+    cursor = 0          # Track the insert location by cursor
+    spinlock_len = 1    # spinlock length matters, but not all values
+    last_code_after_zero = 0
+    for code in range(1, cap + 1):
+        # Check if a new value is inserted after 0, which is always be cursor 0
+        if cursor == 0:
+            last_code_after_zero = code
+        spinlock_len += 1
+        cursor = (cursor + 1 + steps) % spinlock_len
+    return last_code_after_zero
