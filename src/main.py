9e5386a2e2719c7c3468aa21af2dd2965f1492d7
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-08-08 21:09:24 +1000

    Solve Day 17 Part 1

--- src/main.py
+++ src/main.py
@@ -212,6 +212,19 @@ def solve_day16():
     print("==========")
 
 
+def solve_day17():
+    """
+    Solves AOC 2017 Day 17 Parts 1 and 2, printing out the solutions.
+    """
+    print("AOC 2017 Day 17 - \"Spinlock\"")
+    input_data = day17.process_input_file()
+    p1_solution = day17.solve_part1(input_data)
+    print(f"> P1 solution - {p1_solution}")
+    p2_solution = day17.solve_part2(input_data)
+    print(f"> P2 solution - {p2_solution}")
+    print("==========")
+
+
 if __name__ == "__main__":
     # Import to allow execution from project top-level directory
     import os
@@ -219,7 +232,7 @@ if __name__ == "__main__":
     sys.path.append(os.getcwd())
     # Solution module imports
     from src.solutions import day01, day02, day03, day04, day05, day06, day07, \
-        day08, day09, day10, day11, day12, day13, day14, day15, day16
+        day08, day09, day10, day11, day12, day13, day14, day15, day16, day17
     # Main solver methods
     print("==========")
     solve_day01()
@@ -238,3 +251,4 @@ if __name__ == "__main__":
     solve_day14()
     solve_day15()
     solve_day16()
+    solve_day17()
