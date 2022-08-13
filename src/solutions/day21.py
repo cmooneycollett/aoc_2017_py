e976a744a640843850b5016158ff86da317e90dc
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-08-13 15:27:30 +1000

    Refactor Day 21 solution for readability

--- src/solutions/day21.py
+++ src/solutions/day21.py
@@ -2,7 +2,8 @@
 Solutions for AOC 2017 Day 21 - "Fractal Art".
 """
 
-import numpy
+import numpy as np
+
 
 def process_input_file(filepath="./input/day21.txt"):
     """
@@ -40,81 +41,69 @@ def solve_part2(rules):
     artgrid = generate_art(rules, 18)
     return (artgrid == b"#").sum()
 
+
+def apply_enhancement_rule(rules, old_artgrid, old_unit, new_unit):
+    """
+    Applies a single iteration of the enhancement rules to the old artgrid,
+    returning the new artgrid at the expanded size.
+    """
+    # Initialise new artgrid as empty
+    new_width = (old_artgrid.shape[0] // old_unit) * new_unit
+    new_artgrid = np.empty((new_width, new_width), dtype="S1")
+    # Consider each square sub-grid with width of old_unit
+    for row in range(0, old_artgrid.shape[0], old_unit):
+        for col in range(0, old_artgrid.shape[0], old_unit):
+            # Create the window grid
+            windowgrid = np.empty((old_unit, old_unit), dtype="S1")
+            for winx in range(old_unit):
+                for winy in range(old_unit):
+                    windowgrid[winx][winy] = old_artgrid[row +
+                                                         winx][col + winy]
+            # Check possible states by combining 90-degree rotations and flips
+            flip_rules = [[], [np.fliplr], [np.fliplr, np.flipud], [np.fliplr]]
+            match_found = False
+            for _ in range(4):
+                windowgrid = np.rot90(windowgrid)
+                # Apply the flip rules to current rotation and check rule match
+                for position_rules in flip_rules:
+                    # Apply flips
+                    for func in position_rules:
+                        windowgrid = func(windowgrid)
+                    # Generate string for transformed window grid
+                    key = ""
+                    for winx in range(old_unit):
+                        for winy in range(old_unit):
+                            key += windowgrid[winx][winy].decode("utf-8")
+                    # Check if transformed window grid matches a rule
+                    if key in rules:
+                        value = rules[key]
+                        # Add the output from matched rule into the new artgrid
+                        for winx in range(new_unit):
+                            for winy in range(new_unit):
+                                i_val = winx * new_unit + winy
+                                new_row = row // old_unit * new_unit + winx
+                                new_col = col // old_unit * new_unit + winy
+                                new_artgrid[new_row][new_col] = value[i_val]
+                        match_found = True
+                        break
+                if match_found:
+                    break
+                # Put windowgrid back to state before flips were applied
+                windowgrid = np.fliplr(windowgrid)
+                windowgrid = np.flipud(windowgrid)
+    return new_artgrid
+
+
 def generate_art(rules, iterations):
     """
     Generates the art array by applying the specified number of iterations of
     the rules to the starting pattern (3x3): ".#./..#/###"
     """
-    artgrid = numpy.array([[".", "#", "."], [".", ".", "#"], ["#", "#", "#"]], dtype="S1")
+    artgrid = np.array([[".", "#", "."], [".", ".", "#"], ["#", "#", "#"]],
+                       dtype="S1")
     for _ in range(iterations):
         if artgrid.shape[0] % 2 == 0:
-            new_width = (artgrid.shape[0] // 2) * 3
-            new_artgrid = numpy.empty((new_width, new_width), dtype="S1")
-            for row in range(0, artgrid.shape[0], 2):
-                for col in range(0, artgrid.shape[0], 2):
-                    # Create the window grid
-                    windowgrid = numpy.empty((2, 2), dtype="S1")
-                    for delta_y in range(2):
-                        for delta_x in range(2):
-                            windowgrid[delta_y][delta_x] = artgrid[row + delta_y][col + delta_x]
-                    # For each rotation, check if a rule is matched
-                    flip_rules = [[], [numpy.fliplr], [numpy.fliplr, numpy.flipud], [numpy.fliplr]]
-                    match_found = False
-                    for _ in range(4):
-                        windowgrid = numpy.rot90(windowgrid)
-                        for position_rules in flip_rules:
-                            for func in position_rules:
-                                windowgrid = func(windowgrid)
-                            key = ""
-                            for delta_y in range(2):
-                                for delta_x in range(2):
-                                    key += windowgrid[delta_y][delta_x].decode("utf-8")
-                            if key in rules:
-                                value = rules[key]
-                                for delta_y in range(3):
-                                    for delta_x in range(3):
-                                        value_index = delta_y * 3 + delta_x
-                                        new_artgrid[row // 2 * 3 + delta_y][col // 2 * 3 + delta_x] = value[value_index]
-                                match_found = True
-                                break
-                        if match_found:
-                            break
-                        windowgrid = numpy.fliplr(windowgrid)
-                        windowgrid = numpy.flipud(windowgrid)
-            artgrid = new_artgrid
+            artgrid = apply_enhancement_rule(rules, artgrid, 2, 3)
         elif artgrid.shape[0] % 3 == 0:
-            new_width = (artgrid.shape[0] // 3) * 4
-            new_artgrid = numpy.empty((new_width, new_width), dtype="S1")
-            for row in range(0, artgrid.shape[0], 3):
-                for col in range(0, artgrid.shape[0], 3):
-                    # Create the window grid
-                    windowgrid = numpy.empty((3, 3), dtype="S1")
-                    for delta_y in range(3):
-                        for delta_x in range(3):
-                            windowgrid[delta_y][delta_x] = artgrid[row + delta_y][col + delta_x]
-                    # For each rotation, check if a rule is matched
-                    flip_rules = [[], [numpy.fliplr], [numpy.fliplr, numpy.flipud], [numpy.fliplr]]
-                    match_found = False
-                    for _ in range(4):
-                        windowgrid = numpy.rot90(windowgrid)
-                        for position_rules in flip_rules:
-                            for func in position_rules:
-                                windowgrid = func(windowgrid)
-                            key = ""
-                            for delta_y in range(3):
-                                for delta_x in range(3):
-                                    key += windowgrid[delta_y][delta_x].decode("utf-8")
-                            if key in rules:
-                                value = rules[key]
-                                for delta_y in range(4):
-                                    for delta_x in range(4):
-                                        value_index = delta_y * 4 + delta_x
-                                        new_artgrid[row // 3 * 4 + delta_y][col // 3 * 4 + delta_x] = value[value_index]
-                                match_found = True
-                                break
-                        if match_found:
-                            break
-                        windowgrid = numpy.fliplr(windowgrid)
-                        windowgrid = numpy.flipud(windowgrid)
-            artgrid = new_artgrid
+            artgrid = apply_enhancement_rule(rules, artgrid, 3, 4)
     return artgrid
